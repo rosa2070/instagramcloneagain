a98713a1faea378711a98cd5f083e0cfd8d97bbc
jaeeunhyun <rosa2070@naver.com>
2021-01-15 02:31:47 +0900

    print("two")

--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
@@ -19,6 +19,14 @@ class PhotoUpdate(UpdateView):
     fields = ['text', 'image']
     template_name_suffix = '_update'
     success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
 
 class PhotoDelete(DeleteView):
     model = Photo
